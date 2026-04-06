0099a04a7f3f751c7bb5791c6fbf9e65c3d2265e
Faheem Anis <faheem@cartzlink.com>
2026-04-06 18:40:40 +0500

    Added serve command for console

---
+++ src/commands/ServeCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace framework\console\commands;
+
+use framework\Application;
+use framework\console\Command;
+
+class ServeCommand extends Command
+{
+    public string $signature = 'serve';
+
+    public function execute(Application $app, array $args): void
+    {
+        $host = '127.0.0.1';
+        $port = 8000;
+        $docRoot = getcwd();
+
+        echo "Starting PHP server at http://$host:$port\n";
+        echo "Document root: $docRoot\n";
+        echo "Press Ctrl+C to stop\n\n";
+
+        $cmd = sprintf(
+            'php -S %s:%d -t %s',
+            $host,
+            $port,
+            escapeshellarg($docRoot)
+        );
+
+        // Passthru keeps output interactive (important!)
+        passthru($cmd);
+
+    }
+}
